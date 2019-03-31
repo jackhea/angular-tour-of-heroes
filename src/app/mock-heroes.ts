84f12f56cb15e60fc102f6002834c5c921ee5fbb
jackhea <kbpmvb@hotmail.com>
2019-03-31 03:13:32 -0600

    Added Hero class and HeroesComponent, displays a Hero list.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import  { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Outworld Destroyer' },
+  { id: 12, name: 'Pudge' },
+  { id: 13, name: 'Puck' },
+  { id: 14, name: 'Dragon Knight' },
+  { id: 15, name: 'Lina' },
+  { id: 16, name: 'Shadow Fiend' },
+  { id: 17, name: 'Enchantress' },
+  { id: 18, name: 'Luna' },
+  { id: 19, name: 'Clockwerk' },
+  { id: 20, name: 'Crystal Maiden' },
+]
